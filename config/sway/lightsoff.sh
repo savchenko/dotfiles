63c1f9c974e98e0a536997a910a65ce2ba2809d0
Andrew Savchenko <andrew@savchenko.net>
2021-12-01 14:02:42 +1030

    Update Sway config(s)

--- config/sway/lightsoff.sh
+++ config/sway/lightsoff.sh
@@ -1,1 +1,2 @@
 light -s sysfs/leds/tpacpi::power -S 0.00
+light -s sysfs/leds/platform::micmute -S 0
--- config/sway/pa_mono.sh
+++ config/sway/pa_mono.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-M=$(pacmd list-sources | grep -P "name:.*alsa.*input" | tail -n 1 | perl -pe 's/.*<//g;s/>.*//g')
+M=$(pacmd list-sources | grep -P "name:.*alsa.*input" | head -n 1 | perl -pe 's/.*<//g;s/>.*//g')
 pacmd load-module module-remap-source source_name=in_mono master="$M" master_channel_map=front-left channel_map=mono
 pactl set-default-source in_mono
